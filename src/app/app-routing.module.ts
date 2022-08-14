b1882b9bf0c144f7831ac5cbaa316cfcc8920e1f
César Rincón Muñoz <c3z4r.rincon@gmail.com>
2022-08-14 11:54:52 -0500

    Change the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+// import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+// import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+// import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  // { path: 'create-employee', component: EmployeeCreateComponent },
+  // { path: 'employees-list', component: EmployeeListComponent },
+  // { path: 'edit-employee/:id', component: EmployeeEditComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
