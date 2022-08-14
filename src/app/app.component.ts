0795b569d33e1211a2341a60fb1f191733da1142
César Rincón Muñoz <c3z4r.rincon@gmail.com>
2022-08-14 11:54:13 -0500

    Add Employee Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './models/employee';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hometuls-beta';
+  employees: Employee[] = [];
+
+  constructor(
+    private employeeService: EmployeeService
+  ) { }
+
+  ngOnInit(): void {
+    this.employeeService.getAllEmployees()
+    .subscribe(data => {
+      this.employees = data;
+    });
+  }
+
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllEmployees()
+  {
+    return this.http.get<Employee[]>('http://localhost:4000/api')
+  }
+
+}
